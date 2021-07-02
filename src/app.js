d763acd644567ff32b11125a3bdd9ed8ce792b9b
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-02 11:28:36 -0600

    practiced middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
+const sayHello = (req, res, next) => {
+  res.send('Hello!');
+}
+
+app.use(morgan('dev'));
+
+// const logging = (req, res, next) => {
+//   console.log('A request is being made!');
+//   next();
+// }
+
+// app.use(logging);
+app.use(sayHello);
 
 module.exports = app;
